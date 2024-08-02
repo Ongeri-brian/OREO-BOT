751c7104e333e3d4ce754347715cd821868b33b7
Ongeri-brian <brianongeri58@gmail.com>
2024-08-02 19:44:59 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ import fs from 'fs'
 
 //OwnerShip
 global.owner = [
-  [process.env.OWNER_NUMBER || '919637987574', process.env.OWNER_NAME || 'Developer Shizo 🤖', true],
-  ['919637987574', 'Shizo Techie ❤️✨', true]
+  [process.env.OWNER_NUMBER || '0713145618', process.env.OWNER_NAME || 'Developer Shizo 🤖', true],
+  ['919637987574', 'Brian Ongeri', true]
 ]
 global.mods = []
 global.prems = []
